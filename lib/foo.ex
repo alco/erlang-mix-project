6620b4dc02d45b5dfa50ada27b41d7069b476b51
Alexei Sholik <alcosholik@gmail.com>
2015-04-23 15:25:26 +0300

    Add an example of calling Elixir code from Erlang

---
+++ lib/foo.ex
@@ -0,0 +1,5 @@
+defmodule Foo do
+  def reverse_string(str) do
+    String.reverse(str)
+  end
+end
--- src/testproj.erl
+++ src/testproj.erl
@@ -1,6 +1,9 @@
 -module(testproj).
 
--export([start/2]).
+-export([start/2, reverse/1]).
 
 start(_Type, _Args) ->
     {ok,self()}.
+
+reverse(Bin) ->
+    'Elixir.Foo':reverse_string(Bin).
